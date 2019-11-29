eb6fc8135a78ee033404b91ea4a2aa31e3379bba
Jose Alonso <jalonsoriveiro@danielcastelao.org>
2019-11-29 14:04:37 +0100

    Variable cambiadas clase numeros

--- numeros.java
+++ numeros.java
@@ -14,40 +14,40 @@ public class NumerosPrimos {
     //LIstar los numeros según el numero de digitos indicado
     //Considero solo hasta numeros menores a 100000 (5 digitos), 
     //por el hecho de k buscar numeros primos a partir de 6 digitos, el proceso se hace muy lento.
-    public static boolean p = false;
+    public static boolean verificar = false;
 
     public static void main(String arg[]) {
-        int nD = 0;
+        int numUsuario = 0;
         int nd = 0;
-        nD = Integer.parseInt(arg[0]);
-        if (nD <= 0) {
+        numUsuario = Integer.parseInt(arg[0]);
+        if (numUsuario <= 0) {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
-        for (int i = 1; i <= 99999; i++) {
-            int div = i;
-            int c = 0;
+        for (int contador = 1; contador <= 99999; contador++) {
+            int valorContador = contador;
+            int contador_W = 0;
 
-            while (div != 0) {
-                div = div / 10;
-                c++;
+            while (valorContador != 0) {
+                valorContador = valorContador / 10;
+                contador_W++;
             }
-            nd = c;
+            nd = contador_W;
 
-            if (nd == nD) {
-                if (i < 4) {
-                    p = true;
+            if (nd == numUsuario) {
+                if (contador < 4) {
+                    verificar = true;
                 } else {
-                    if (i % 2 == 0) {
-                        p = false;
+                    if (contador % 2 == 0) {
+                        verificar = false;
                     } else {
                         int c1 = 0;
                         int i1 = 1;
-                        int l = (i - 1) / 2;
+                        int l = (contador - 1) / 2;
                         if (l % 2 == 0) {
                             l--;
                         }
                         while (i1 <= l) {
-                            if (i % i1 == 0) {
+                            if (contador % i1 == 0) {
                                 c1++;
                             }
                             i1 += 2;
@@ -57,13 +57,13 @@ public class NumerosPrimos {
                         }
 
                         if (c1 == 1) {
-                            p = true;
+                            verificar = true;
                         }
                     }
                 }
 
-                if (p == true) {
-                    System.out.println(i);
+                if (verificar == true) {
+                    System.out.println(contador);
                 }
             }
         }
